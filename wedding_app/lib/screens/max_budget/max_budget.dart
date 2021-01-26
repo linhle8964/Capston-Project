b6089fd180b29b02b811312be5527fa7d40026d8
braveguy12 <52749048+braveguy12@users.noreply.github.com>
2021-01-26 13:35:12 +0700

    create maxbudget screen

---
+++ wedding_app/lib/screens/max_budget/max_budget.dart
@@ -0,0 +1,88 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MaxBudget());
+}
+
+class MaxBudget extends StatefulWidget {
+  @override
+  _MaxBudgetState createState() => _MaxBudgetState();
+}
+
+class _MaxBudgetState extends State<MaxBudget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: null,
+            ),
+            title: Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 30, 0),
+              child: Text(
+                "TỔNG CHI PHÍ",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          body: Container(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 70, 0, 0),
+                  child: Center(
+                    child: Container(
+                      child: Text('Tổng chi phí hiện tại là:',style: TextStyle(fontSize: 25),),
+                    ),
+                  )
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child:  Center(
+                    child: Image.asset('assets/budget.png',height: 200.0,),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(70, 20, 30, 0),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('200.000.000 đ',style: TextStyle(fontSize: 25),),
+                        FlatButton(
+                            onPressed: onEditClick,
+                            child: Image.asset('assets/edit.png',height: 50.0,),)
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: RaisedButton(
+                      color: Colors.blue,
+                      onPressed: onSaveClick,
+                      child: Text(
+                        'Lưu',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ));
+  }
+}
+void onEditClick(){}
+void onSaveClick(){}
