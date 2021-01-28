616231066bbe7ca079fd1c1752a054b11dd9b58b
braveguy12 <52749048+braveguy12@users.noreply.github.com>
2021-01-28 11:10:43 +0700

    Update wedding_date.dart

--- wedding_app/lib/screens/wedding_date/wedding_date.dart
+++ wedding_app/lib/screens/wedding_date/wedding_date.dart
@@ -7,82 +7,108 @@ void main() {
   runApp(WeddingDate());
 }
 
-class WeddingDate extends StatefulWidget {
-  @override
-  _WeddingDateState createState() => _WeddingDateState();
-}
-
-class _WeddingDateState extends State<WeddingDate> {
+class WeddingDate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              leading: IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: null,
-              ),
-              title: Padding(
-                padding: const EdgeInsets.fromLTRB(60, 0, 30, 0),
-                child: Text(
-                  "NGÀY CƯỚI",
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-            ),
-            body: Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 70, 0, 0),
-                      child: Center(
-                        child: Container(
-                          child: Text('Ngày cưới dự định:',style: TextStyle(fontSize: 25),),
-                        ),
-                      )
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child:  Center(
-                      child: Image.asset('assets/calender.jpg',height: 200.0,),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(80, 20, 40, 0),
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text('19-07-2021',style: TextStyle(fontSize: 25),),
-                          FlatButton(
-                            onPressed: onEditClick,
-                            child: Image.asset('assets/edit.png',height: 50.0,),)
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: RaisedButton(
-                        color: Colors.blue,
-                        onPressed: onSaveClick,
-                        child: Text(
-                          'Lưu',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-        ));
+      title: 'WeddingDate',
+      home: WeddingDateMain(),
+    );
   }
 }
-void onEditClick(){}
+
+class WeddingDateMain extends StatefulWidget {
+  @override
+  _WeddingDateMainState createState() => _WeddingDateMainState();
+}
+
+class _WeddingDateMainState extends State<WeddingDateMain> {
+  DateTime selectedDate = DateTime.now();
+
+  _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: null,
+          ),
+          title: Padding(
+            padding: const EdgeInsets.fromLTRB(60, 0, 30, 0),
+            child: Text(
+              "NGÀY CƯỚI",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ),
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 70, 0, 0),
+                  child: Center(
+                    child: Container(
+                      child: Text('Ngày cưới dự định:',style: TextStyle(fontSize: 25),),
+                    ),
+                  )
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child:  Center(
+                  child: Image.asset('assets/calender.jpg',height: 200.0,),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(120, 20, 30, 0),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "${selectedDate.toLocal()}".split(' ')[0],
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      FlatButton(
+                        onPressed: () => _selectDate(context),
+                        child: Image.asset('assets/edit.png',height: 50.0,),)
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    onPressed: onSaveClick,
+                    child: Text(
+                      'Lưu',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
 void onSaveClick(){}
