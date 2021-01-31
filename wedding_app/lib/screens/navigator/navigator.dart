12568891341711d826a5b6c50e935387dbe74f90
NangLD290498 <nangld290498@gmail.com>
2021-01-31 21:58:35 +0700

    commit main changes

--- wedding_app/lib/screens/navigator/navigator.dart
+++ wedding_app/lib/screens/navigator/navigator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wedding_app/demo_page.dart';
+import 'package:wedding_app/screens/checklist/checklist_page.dart';
+import 'package:wedding_app/screens/checklist/edit_task.dart';
 import 'package:wedding_app/screens/personal_info/personal_info.dart';
 import 'package:wedding_app/screens/setting/setting.dart';
 import 'package:wedding_app/screens/wedding_date/wedding_date.dart';
@@ -15,7 +17,7 @@ class _NavigatorPageState extends State<NavigatorPage> {
     DemoPage(),
     SettingPage(),
     PersonalInfoPage(),
-    WeddingDatePage(),
+    EditTask(),
   ];
   void onTabTapped(int index) {
     setState(() {
