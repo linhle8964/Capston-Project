44a96697184d237bedc5230eb1a6d2c09fef83ac
braveguy12 <52749048+braveguy12@users.noreply.github.com>
2021-04-26 22:15:21 +0700

    Update chooseTemplate_page.dart

--- wedding_app/lib/screens/choose_template_invitation/chooseTemplate_page.dart
+++ wedding_app/lib/screens/choose_template_invitation/chooseTemplate_page.dart
@@ -526,7 +526,7 @@ class MyCardState extends State<MyCard> {
             }
             if (state is InvitationCardLoading) {
               return Padding(
-                padding: const EdgeInsets.fromLTRB(140, 200, 50, 0),
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                 child: Container(child: LoadingIndicator()),
               );
             } else if (state is InvitationCardLoaded) {
