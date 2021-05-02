39150684e0aa1c9e1464dfffbb0e95a1297f5473
linhle8964 <linhle8964@users.noreply.github.com>
2021-05-02 22:28:04 +0700

    change share invitation link

--- wedding_app/lib/utils/share_guest_response_link.dart
+++ wedding_app/lib/utils/share_guest_response_link.dart
@@ -3,7 +3,7 @@ import 'package:share/share.dart';
 
 void shareGuestResponseLink(BuildContext context, String weddingId) async {
   final RenderBox box = context.findRenderObject();
-  await Share.share("https://capston-project-d06da.web.app/guest/$weddingId",
+  await Share.share("https://nangld290498.github.io/VVWED/guest/$weddingId",
       subject: "Xin hãy nhấn vào link này để gửi phản hồi",
       sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
 }
